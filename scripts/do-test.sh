21196a3d84e7a1a2d438ebbf187078d985423ccb
Maksym Sobolyev <sobomax@sippysoft.com>
2019-11-15 14:11:04 -0800

    Test with -w option as well.

--- scripts/do-test.sh
+++ scripts/do-test.sh
@@ -12,8 +12,8 @@ make all
 ./configure --enable-coverage
 make clean all
 cd src
-for arg in '' '-p'
+for arg in '' '-w' '-p' '-p -w'
 do
   ./testskew ${arg} -L 200 5
+  sh ./findskew.sh ${arg}
 done
-sh -x ./findskew.sh
