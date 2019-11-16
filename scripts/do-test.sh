f16df98f9c71d3a08965ad37959579efd608f59b
Maksym Sobolyev <sobomax@sippysoft.com>
2019-11-15 17:44:17 -0800

    Only run low frequency test on x86_64 now.

--- scripts/do-test.sh
+++ scripts/do-test.sh
@@ -17,6 +17,9 @@ for arg in '' '-w' '-p' '-p -w'
 do
   ./testskew ${arg} -L 200 5
   ./testskew ${arg} 10 5.0 20 2.5 40 2.5 80 2.5 160 2.5 320 2.5
-  ./testskew ${arg} -f 0.75 -v 0.1 100
+  if [ "`uname -m`" != "arm64" ]
+  then
+    ./testskew ${arg} -f 0.75 -v 0.1 100
+  fi
   sh ./findskew.sh ${arg}
 done
