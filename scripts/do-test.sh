15ad87c465b19031e1fb351c6325101543543a03
Maksym Sobolyev <sobomax@sippysoft.com>
2019-11-16 12:29:44 -0800

    For completeness sake also test -m option and few bad arguments too.

--- scripts/do-test.sh
+++ scripts/do-test.sh
@@ -20,8 +20,15 @@ then
 fi
 
 cd src
+
+for badargs in '--foobar' '-?' 'x'
+do
+  ./testskew ${badarg} 2>/dev/null && false
+done
+
 for arg in '' '-w' '-p' '-p -w'
 do
+  ./testskew ${arg} -m 997.983 0.1
   ./testskew ${arg} -L 200 5
   ./testskew ${arg} 10 5.0 20 2.5 40 2.5 80 2.5 160 2.5 320 2.5
   ./testskew ${arg} -f 0.75 -v 0.1 100
