a3d3d3d402a27751af030b9e3505482209021f16
Maksym Sobolyev <sobomax@sippysoft.com>
2020-11-29 08:49:45 -0800

    Hack module builder to not add platform tag to module .so name.

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ try:
 except ImportError:
     from distutils.core import setup
 from distutils.core import Extension
-import os
+from distutils.command.build_ext import build_ext
+import os, sysconfig
 
 elp_srcs = ['src/periodic.c', 'src/prdic_math.c', \
  'src/prdic_fd.c', \
@@ -23,6 +24,26 @@ def get_ex_mod():
         return None
     return [module1]
 
+def get_ext_filename_without_platform_suffix(filename):
+    name, ext = os.path.splitext(filename)
+    ext_suffix = sysconfig.get_config_var('EXT_SUFFIX')
+
+    if ext_suffix == ext:
+        return filename
+
+    ext_suffix = ext_suffix.replace(ext, '')
+    idx = name.find(ext_suffix)
+
+    if idx == -1:
+        return filename
+    else:
+        return name[:idx] + ext
+
+class BuildExtWithoutPlatformSuffix(build_ext):
+    def get_ext_filename(self, ext_name):
+        filename = super().get_ext_filename(ext_name)
+        return get_ext_filename_without_platform_suffix(filename)
+
 kwargs = {'name':'ElPeriodic',
       'version':'1.0',
       'description':'Phase-locked userland scheduling library',
@@ -31,7 +52,8 @@ kwargs = {'name':'ElPeriodic',
       'url':'https://github.com/sobomax/libelperiodic',
       'packages':['elperiodic',],
       'package_dir':{'elperiodic':'python'},
-      'ext_modules': get_ex_mod()
+      'ext_modules': get_ex_mod(),
+      'cmdclass': {'build_ext': BuildExtWithoutPlatformSuffix}
      }
 
 if __name__ == '__main__':
